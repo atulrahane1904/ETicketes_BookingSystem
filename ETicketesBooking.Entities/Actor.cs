1c057c36d8e849631ed3f73bf022b574660aa2a0
atul_rahane <atulrahane1904@gmail.com>
2024-12-03 11:08:27 +0530

    start creating entities for project

---
+++ ETicketesBooking.Entities/Actor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicketesBooking.Entities
+{
+    public class Actor
+    {
+        public int ActorId { get; set; }
+
+        public string ActorName { get; set; }
+
+        public string Bio { get; set; }
+
+        public string ProfilePicURL { get; set; }
+
+
+    }
+}
